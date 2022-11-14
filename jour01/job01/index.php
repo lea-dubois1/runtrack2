9dd554809362497420bd63d50f9acd997a05518c
lea-dubois1 <lea.dubois@laplateforme.io>
2022-11-14 20:38:18 +0100

    job01 finis, creation index.php job02

--- jour01/job01/index.php
+++ jour01/job01/index.php
@@ -3,7 +3,16 @@
 $str = "LaPlateforme";
 $str2 = "Vive";
 $str3 = "!";
+echo "$str2 $str $str3";
 
-echo "$str2 $str $str3"
+$val = 6;
+echo $val;
+$val = $val + 4;
+echo $val;
+
+$myBool = true;
+echo $myBool;
+$myBool = false;
+echo $myBool;
 
 ?>
