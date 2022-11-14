e4330074f2cc1c525e8c8175051231b5b6976196
lea-dubois1 <lea.dubois@laplateforme.io>
2022-11-14 20:42:14 +0100

    job02 terminé

---
+++ jour01/job02/index.php
@@ -0,0 +1,9 @@
+<?php
+
+$phrase = "Hello LaPlateforme!";
+echo $phrase; // Ceci est un commentaire sur une ligne
+echo $phrase; /* Ceci est un commentaire
+                sur plusieures lignes */
+
+
+?>
