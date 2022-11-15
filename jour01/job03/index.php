83ff31485846030bf70e7d86988188103b63807b
lea-dubois1 <lea.dubois@laplateforme.io>
2022-11-15 09:24:12 +0100

    job03 terminé

--- jour01/job03/index.php
+++ jour01/job03/index.php
@@ -5,13 +5,36 @@ $myInt = 103;
 $myString = "Hello world!";
 $myFloat = 4.7;
 
+echo "
+<table>
+    <thead>
+        <tr>
+            <th>type</th>
+            <th>nom</th>
+            <th>valeur</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <th>boolean</th>
+            <td>myBool</td>
+            <td>$myBool</td>
+        </tr>
+        <tr>
+            <th>entier</th>
+            <td>myInt</td>
+            <td>$myInt</td>
+        </tr>
+        <tr>
+            <th>chaîne de caractères</th>
+            <td>myString</td>
+            <td>$myString</td>
+        </tr>
+        <tr>
+            <th>nombre à virgule flotante</th>
+            <td>myFloat</td>
+            <td>$myFloat</td>
+        </tr>
+    </tbody>
+</table> "
 ?>
-
-<!DOCTYPE html>
-<html>
-
-    <head>
-
-        
-    </head>
-</html>
