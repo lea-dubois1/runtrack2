6baa162c6826d79ea05dca909b27ce8918f6bf26
lea-dubois1 <lea.dubois@laplateforme.io>
2022-11-15 12:57:25 +0100

    jour02 job02 terminé

---
+++ jour02/job01/index.php
@@ -0,0 +1,15 @@
+<?php
+
+$nb = 0;
+
+while($nb < 1337){
+    if($nb === 42) {
+        echo "<u><b>$nb</b></u><br>";
+        $nb++;
+    }else {
+        echo "$nb<br>";
+        $nb++;
+    }
+}
+
+?>
