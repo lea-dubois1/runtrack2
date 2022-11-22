f5b155625e38aa7af0d4b8002ae5ccf88570fee6
lea-dubois1 <lea.dubois@laplateforme.io>
2022-11-22 11:43:16 +0100

    job06 terminé

--- jour07/job06/index.php
+++ jour07/job06/index.php
@@ -1,24 +1,41 @@
 <?php
 
 $phrase = "Ceci est une longue phrase";
-$lettre = "e";
 
-function occurences($str, $char){
+function leetSpeak($str){
 
-    $nbChar = 0;
+    for ($i=0; isset($str[$i]) ; $i++) {
 
-    for ($i=0; isset($str[$i]) ; $i++) { 
+        if ($str[$i] === 'a' || $str[$i] === 'A'){
+            $str[$i] = '4';   
 
-        if ($str[$i] === "$char") {
-
-            $nbChar++;
+        }elseif ( $str[$i] === 'b' || $str[$i] === 'B'){
+            $str[$i] = '8';
+        
+        }elseif ($str[$i] === 'e' || $str[$i] === 'E'){
+            $str[$i] = '3';
+        
+        }elseif ($str[$i] === 'g' || $str[$i] === 'G'){
+            $str[$i] = '6';
+        
+        }elseif ($str[$i] === 'l' || $str[$i] === 'L'){
+            $str[$i] = '1';
+        
+        }elseif ($str[$i] === 's' || $str[$i] === 'S'){
+            $str[$i] = '5';
+        
+        }elseif ($str[$i] ==='t' || $str[$i] === 'T'){
+            $str[$i] = '7';
+        
+        }else{
+            $str[$i] = $str[$i];
         }
     }
-
-    return $nbChar;
+    
+    return $str;
 }
 
-$nb = occurences($phrase, $lettre);
-echo 'Il y a ' . $nb . ' fois la lettre \'' . $lettre . '\' dans la phrase "' . $phrase . '"'
+$leetStr = leetSpeak($phrase);
+echo $leetStr;
 
 ?>
