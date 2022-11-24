c666954125a44839d169d3bd4a1bf1f82c7b3715
lea-dubois1 <lea.dubois@laplateforme.io>
2022-11-24 08:52:32 +0100

    job03 terminé

--- jour08/job03/index.php
+++ jour08/job03/index.php
@@ -3,19 +3,16 @@
 session_start(); //démarrage de la session
 
 
-
-
 if(isset($_POST['submit'])) { //si $_POST['submit'] existe rentre dans la condition 
-    $_SESSION['user'][] = $_POST['prenom']; //$_POST['prenom'] est stocker dans un tableau dans la varibale $_SESSION['user]
+    $_SESSION['user'][] = $_POST['prenom']; //$_POST['prenom'] est stocké dans un tableau dans la varibale $_SESSION['user']
 }
 
 if(isset($_POST['reset'])) { // si $_POST['reset'] existe rentre dans la condition
     unset($_SESSION['user']); //et unset $_['session user']
 }
 
-
-
 ?>
+
 
 <form method="POST" id="form">
     <input type="text" name="prenom" id="prenom" placeholder="Entrez votre prénom ">
@@ -27,7 +24,7 @@ if(isset($_POST['reset'])) { // si $_POST['reset'] existe rentre dans la conditi
 <?php
 if(isset($_SESSION['user'])) { //si $_SESSION['user'] existe rentre dans la conditions
     foreach($_SESSION['user'] as $values){ //pour chaque value dans $_SESSION['user']
-    echo '<br>'. $values . '</br>'; //afficher values avec un saut de ligne
+        echo '<br>'. $values . '</br>'; //afficher values avec un saut de ligne
     }
 }
 ?>
